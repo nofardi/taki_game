a1749a83e20e29443eb57e2db8adeab006d81398
Yonathan Gross <yonatan2gross@gmail.com>
2018-04-16 18:14:58 +0300

    1. fixed grid 2. setting skeleton for classes: card, player 3. setting game skeleton.

--- taki.js
+++ taki.js
@@ -30,19 +30,47 @@ var gameTypeEnum = {
     PVC: 1,
 };
 
-var player = {
-    cards: [],
-    isComputer: false,
-    currentAmountOfCards: 0
-};
+var players=[];
+var deck=[];
 
-function game() {
+function initGame(){
+    var numOfPlayers=2;
+    var continuePlaying=true;
+    while(!continuePlaying)
+    {
+        newGame(numOfPlayers);
+    }
 
 }
 
-function card(value, color) {
+function newGame(numOfPlayers) {
+    var gameFinished=false;
+    createDeck(); // create the deck and shuffle.
+    createPlayers(numOfPlayers,gameTypeEnum.PVC); // create the players and deal them.
+    while(!gameFinished)
+    {
+        gameFinished= newRound();
+    }
+};
+
+function newRound() {
+    var gameFinished=false;
+
+    var currentPlayerIndex;
+    for(currentPlayerIndex=0;currentPlayerIndex< players.length || !gameFinished;currentPlayerIndex++)
+    {
+
+        players[currentPlayerIndex].playTurn();
+
+    }
+}
+
+
+
+function card(value, color,isWildCard) {
     var value = value;
     var color = color;
+    var isWildCard=isWildCard; // bool
 
     function printValue() {
         console.log(value);
@@ -58,52 +86,85 @@ function card(value, color) {
     this.getColor = function () {
         return color;
     }
-}
-function createDeck() {
 
+    this.isWildCard = function () {
+        return isWildCard;
+    }
+}
+
+function createDeck() {
     var numOfCardsReturn = 2;
-    var deck = [];
     for (var cardValue = 0; cardValue < valuesEnum.length; cardValue++) {
         for (var color = 0; color < colorsEnum.length; color++) { //
             for (var returnIndex = 0; returnIndex < numOfCardsReturn; returnIndex++) { //two of each card.
-                var specialValue = parseInt(valuesEnum[cardValue]);
-                specialValue =
+                var isWildCard = parseInt(valuesEnum[cardValue]);
+                isWildCard =
                     valuesEnum[cardValue] == "Taki" ||
                     valuesEnum[cardValue] == "Stop" ||
                     valuesEnum[cardValue] == "ChangeColor";
-                var card = {Value: valuesEnum[cardValue], Color: colorsEnum[color], IsWildCard: specialValue};
+                var card = new card(valuesEnum[cardValue], colorsEnum[color],isWildCard);
                 deck.push(card);
             }
         }
     }
-}
 
-function deliverCards(amount, player) {
-    for (var i = 0; i < amount; i++) {
-        player.cards.push(deck.pop());
-        player.currentAmountOfCards++;
+    shuffle(deck);
+}
+function player(name,isComputer,ID,Points,Hand,PlayerType,CurrentAmountOfCards)
+{
+    var name=name;
+    var isComputer=isComputer;
+    var ID=ID;
+    var points=Points;
+    var hand=Hand;
+    var playerType=PlayerType;
+    var currentAmountOfCards=CurrentAmountOfCards;
+
+    this.getPoints =function () {
+        return
+    }
+}
+//  Fisher-Yates shuffle
+function shuffle(deck) {
+    var i;
+    var j = 0;
+    var temp = null;
+
+    for (i = deck.length - 1; i > 0; i -= 1) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
     }
 }
 
+
 function createPlayers(numOfPlayers, gameType) {
-    players = [];
     for (var playerIndex = 0; playerIndex < numOfPlayers; playerIndex++) {
         var hand = [];
         var player = {
             Name: 'Player ' + playerIndex,
+            isComputer: false,
             ID: playerIndex,
             Points: 0,
             Hand: hand,
-            PlayerType: null
+            PlayerType: null,
+            currentAmountOfCards: 0,
         };
+        dealHands(player);
         players.push(player);
     }
 
-    initPlayersType(gameType);
-
+    initPlayersType(gameType,numOfPlayers);
 }
-
-function initPlayersType(gameType) {
+function dealHands(player) {
+    var initialCardAmountToPlayer=8;
+    for (var i = 0; i < initialCardAmountToPlayer; i++) {
+        player.hand.push(deck.pop());
+        player.currentAmountOfCards++;
+    }
+}
+function initPlayersType(gameType,numOfPlayers) {
     players[0].playerType = playerTypeEnum.Human;
 
     if (gameType == gameTypeEnum.PVC) {// Player vs Computer
@@ -121,34 +182,6 @@ function initPlayersType(gameType) {
     }
 }
 
-//  Fisher-Yates shuffle
-function shuffle(deck) {
-    var i;
-    var j = 0;
-    var temp = null;
-
-    for (i = deck.length - 1; i > 0; i -= 1) {
-        j = Math.floor(Math.random() * (i + 1));
-        temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
-    }
-}
-
-function dealHands() {
-    var startingNumberOfCards = 8;
-    // alternate handing cards to each player
-    for (var i = 0; i < startingNumberOfCards; i++) {
-        for (var playerIndex = 0; playerIndex < players.length; playerIndex++) {
-            var card = deck.pop();
-            players[playerIndex].Hand.push(card);
-            renderCard(card, playerIndex);
-            // updatePoints();
-        }
-    }
-
-    updateDeck();
-}
 
 function exit_button_clicked() {
     alert("You've exited the game!");
