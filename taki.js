27895856d0514fb107f0ee1555862f66ff2ffcc8
Yonathan Gross <yonatan2gross@gmail.com>
2018-04-16 16:17:51 +0300

    1. adding grid 2. adding modules

--- taki.js
+++ taki.js
@@ -1,27 +1,177 @@
-function exit_button_clicked() {
-	alert("You've exited the game!");
+var colorsEnum = {
+    "Green": 1,
+    "Yellow": 2,
+    "Red": 3,
+    "Blue": 4,
+};
+
+var valuesEnum = {
+    // numbers
+    'ONE': 1,
+    'THREE': 3,
+    'FOUR': 4,
+    'FIVE': 5,
+    'SIX': 6,
+    'SEVEN': 7,
+    'EIGHT': 8,
+    'NINE': 9,
+
+    // special cards
+    'TAKI': 10,
+    'STOP': 11,
+    'CHANGE_COLOR': 12,
+};
+
+var playerTypeEnum = {
+    Human: 1,
+    Computer: 2,
+};
+var gameTypeEnum = {
+    PVC: 1,
+};
+
+
+var players = new Array();
+
+function game() {
+
 }
 
-var colors = ["Green", "Yellow", "Red", "Blue"];
-var values = ["1", "3", "4", "5", "6", "7", "8", "9", "Taki", "Stop", "ChangeColor"];
-var deck = new Array();
-var players = new Array();
-var currentPlayer = 0;
+function card(value, color) {
+    var value = value;
+    var color = color;
+
+    function printValue() {
+        console.log(value);
+    }
+
+    function printColor() {
+        console.log(color);
+    }
+
+    this.getValue = function () {
+        return value;
+    }
+    this.getColor = function () {
+        return color;
+    }
+};
+
+
+var player = {
+    cards: [],
+    isComputer: false,
+    currentAmountOfCards: 0
+}
 
 function createDeck() {
-    deck = new Array();
-    for (var cardValue = 0; cardValue < values.length; cardValue++) {
-        for (var color = 0; color < colors.length; color++) {
-            for (var i = 0; i < 2; i++) {
-                //TODO: no weight needed, what should we do instead?
-                /*var weight = parseInt(values[cardValue]);
-                if (values[cardValue] == "J" || values[cardValue] == "Q" || values[cardValue] == "K")
-                    weight = 10;
-                if (values[cardValue] == "A")
-                    weight = 11;*/
-                var card = {Value: values[cardValue], Suit: suits[color], Weight: weight};
+
+    var numOfCardsReturn = 2;
+    var deck = new Array();
+    for (var cardValue = 0; cardValue < valuesEnum.length; cardValue++) {
+        for (var color = 0; color < colorsEnum.length; color++) { //
+            for (var returnIndex = 0; returnIndex < numOfCardsReturn; returnIndex++) { //two of each card.
+                var specialValue = parseInt(valuesEnum[cardValue]);
+                specialValue =
+                    valuesEnum[cardValue] == "Taki" ||
+                    valuesEnum[cardValue] == "Stop" ||
+                    valuesEnum[cardValue] == "ChangeColor";
+                var card = {Value: valuesEnum[cardValue], Color: colorsEnum[color], IsWildCard: specialValue};
                 deck.push(card);
             }
         }
     }
 }
+
+function deliverCards(amount, player) {
+    for (var i = 0; i < amount; i++) {
+        player.cards.push(deck.pop());
+        player.currentAmountOfCards++;
+    }
+}
+
+function createPlayers(numOfPlayers, gameType) {
+    players = new Array();
+    for (var playerIndex = 0; playerIndex < numOfPlayers; playerIndex++) {
+        var hand = new Array();
+        var player = {
+            Name: 'Player ' + playerIndex,
+            ID: playerIndex,
+            Points: 0,
+            Hand: hand,
+            PlayerType: null
+        };
+        players.push(player);
+    }
+
+    initPlayersType(gameType);
+
+}
+
+function initPlayersType(gameType) {
+    players[0].playerType = playerTypeEnum.Human;
+
+    if (gameType == gameTypeEnum.PVC) {// Player vs Computer
+
+        // init the rest of the players to Computer.
+        for (var playerIndex = 1; playerIndex < numOfPlayers; playerIndex++) {
+            players[playerIndex].playerType = playerTypeEnum.Computer;
+        }
+    }
+    else {                       // Player vs Players.
+
+        for (var playerIndex = 1; playerIndex < numOfPlayers; playerIndex++) {
+            players[playerIndex].playerType = playerTypeEnum.Human;
+        }
+    }
+}
+
+//  Fisher-Yates shuffle
+function shuffle(deck) {
+    var i;
+    var j = 0;
+    var temp = null;
+
+    for (i = deck.length - 1; i > 0; i -= 1) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+}
+
+function dealHands() {
+    var startingNumberOfCards = 8;
+    // alternate handing cards to each player
+    for (var i = 0; i < startingNumberOfCards; i++) {
+        for (var playerIndex = 0; playerIndex < players.length; playerIndex++) {
+            var card = deck.pop();
+            players[playerIndex].Hand.push(card);
+            renderCard(card, playerIndex);
+            // updatePoints();
+        }
+    }
+
+    updateDeck();
+}
+
+function updateDeck() {
+    document.getElementById('deckcount').innerHTML = deck.length;
+}
+
+function renderCard(card, player) {
+    var hand = document.getElementById('hand_' + player);
+    hand.appendChild(getCardUI(card));
+}
+
+function getCardUI(card) {
+    var el = document.createElement('div');
+    el.className = 'card';
+    el.innerHTML = card.Color + ' ' + card.Value;
+    return el;
+}
+
+function exit_button_clicked() {
+    alert("You've exited the game!");
+}
+
