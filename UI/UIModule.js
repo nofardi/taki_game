f9c7d9e35c8ec50ac723d8c3d7bc9b091683decc
Yonathan Gross <yonatan2gross@gmail.com>
2018-04-24 15:28:10 +0300

    adding flippedCard class and todo's.

--- UI/UIModule.js
+++ UI/UIModule.js
@@ -1,18 +1,23 @@
 var uiModule = (function () {
-
     return {
-        populateDeck: function(){
-            var flippedCard=document.createElement("img");
-            var drawPile=document.getElementsByClassName("draw-pile")[0];
-            flippedCard.src="Resources/FlippedCard.png"
-            drawPile.appendChild(flippedCard)
+        populateDeck: function () {
+            var flippedCard = document.createElement("img");
+            var drawPile = document.getElementsByClassName("draw-pile")[0];
+            flippedCard.src = "Resources/FlippedCard.png" ;
+            flippedCard.className="flipped-card";
+            //TODO: need to set the other attributes appropriately.
+
+            drawPile.appendChild(flippedCard);
+            //TODO: need to stack a bunch of cards on each other.
         },
 
         populatePlayers: function (players) {
+            players.forEach((player) => {
+                //TODO: populate players according to playerType.
+                //TODO: populate players
 
+            })
         }
-
-
     }
 
 })();
